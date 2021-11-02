a3338de593c742637454c4d995d7a0bbd6a3987d
Adit Dhawan <aditdhawan@192.168.1.3>
2021-11-02 22:15:12 +0530

    Added movies to the database and linked the actors and directors to the movies. The genre model hasnt been attached yet. decriptions and runtime have not been filled

--- src/movies/models.py
+++ src/movies/models.py
@@ -6,23 +6,36 @@ from django.db.models.fields import CharField
 #Movie.objects.create(title=data[i]['title'], rating=data[i]['rating'],year=data[i]['year'], genre= ','.join(data[i]['genre']),actors=','.join(data[i]['actors']),directors=','.join(data[i]['directors']),runtime = data[i]['runtime'], certificate = data[i]['certificate'], icon_url=data[i]['icon_url'],big_poster_url = data[i]['big_poster_url'],poster_url = data[i]['poster_url'])
 #genre
 
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Movie(models.Model):
     title       = models.CharField(max_length=100)
-    runtime     = models.IntegerField()
+    runtime     = models.IntegerField(blank=True, null=True)
     year        = models.IntegerField()
     description = models.TextField(null =True)
     rating      = models.DecimalField(decimal_places=1,max_digits=2)
-    genre       = models.TextField(null=True)
-    directors   = models.TextField()
-    actors      = models.TextField()
+    genre       = models.ManyToManyField(Genre)
+    directors   = models.ManyToManyField(Director)
+    actors      = models.ManyToManyField(Actor)
     certificate = models.TextField(max_length=4)
     icon_url    = models.SlugField(max_length=220, null=True)
     poster_url  = models.SlugField(max_length=220, null=True)
     big_poster_url = models.SlugField(max_length=220, null=True)
 
-class Actor(models.Model):
-    name = models.CharField(max_length=100)
-
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
