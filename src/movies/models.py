e69bd2c7c7d89a4ff41ba8f86e345391082db66e
Adit Dhawan <aditdhawan@Vikrams-MacBook-Air-2.local>
2021-11-02 11:13:26 +0530

    added Artist and genre to model

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,5 +1,7 @@
 from typing_extensions import runtime
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
 
 #Movie.objects.create(title=data[i]['title'], rating=data[i]['rating'],year=data[i]['year'], genre= ','.join(data[i]['genre']),actors=','.join(data[i]['actors']),directors=','.join(data[i]['directors']),runtime = data[i]['runtime'], certificate = data[i]['certificate'], icon_url=data[i]['icon_url'],big_poster_url = data[i]['big_poster_url'],poster_url = data[i]['poster_url'])
 #genre
@@ -9,6 +11,7 @@ class Movie(models.Model):
     title       = models.CharField(max_length=100)
     runtime     = models.IntegerField()
     year        = models.IntegerField()
+    description = models.TextField(null =True)
     rating      = models.DecimalField(decimal_places=1,max_digits=2)
     genre       = models.TextField(null=True)
     directors   = models.TextField()
@@ -17,3 +20,9 @@ class Movie(models.Model):
     icon_url    = models.SlugField(max_length=220, null=True)
     poster_url  = models.SlugField(max_length=220, null=True)
     big_poster_url = models.SlugField(max_length=220, null=True)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
